4183caeeef34e8f6f6945e1de77e03bccf95609f
DiveshSachdeva12 <diveshkumarsachdeva85@gmail.comm>
2024-07-27 20:27:52 +0530

    adding variables and datatypes

---
+++ Javascript/lecture-1/variable & datatypes/app.js
@@ -0,0 +1,57 @@
+// there are 7 primitives
+
+// primitive data type in js 
+// undefine :
+// data type (Represents a variable that has been declared but not yet assigned a value)
+
+// let a; 
+
+
+// -----------------------------------------
+// string
+// let name="Divesh1";
+// let name1='Divesh2';
+// let name2=`Divesh3`; /* Backticks */
+
+// console.log(name);
+// console.log(name1);
+// console.log(name2);
+
+
+// ------------------------------------
+// Null : (It’s a special value assigned to variables to denote that they have no value.)
+
+// let a=null;
+// console.log(a);
+
+// ------------------------------------
+
+// boolean Represents a logical value that can be either true or false.
+// let istrue=true;
+// console.log(istrue);
+
+// let isfalse=false;
+// console.log(isfalse);
+
+
+// ------------------------------------
+// 5. Number
+// Represents both integer and floating-point numbers. JavaScript uses a double-precision 64-bit format for all numbers.
+
+// let integer=45
+// console.log(integer);
+
+// let floatingpoint=3.14;
+// console.log(floatingpoint);
+
+
+// ------------------------------------
+// 6. BigInt
+
+// let bignumber=1232131243434324342343241241509;
+// console.log(bignumber)
+
+// ------------------------------------
+// 7. Symbol
+
+
